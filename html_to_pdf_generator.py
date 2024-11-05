5f7bc352d6611125d81c36eb3eb470e91e91cff1
Cozoob <kozubmarcin10@gmail.com>
2024-11-05 17:16:41 +0100

    Try save file to S3

--- html_to_pdf_generator.py
+++ html_to_pdf_generator.py
@@ -5,7 +5,7 @@ import subprocess
 from pyhtml2pdf import converter
 import json
 import logging
-# import boto3
+import boto3
 
 logger = logging.getLogger()
 logger.setLevel("INFO")
@@ -22,17 +22,17 @@ def generate_pdf_from_html():
     # logger.info("Started lambda function")
     # message = 'HELLO FROM LAMBDA'
 
-    # s3 = boto3.resource("s3")
-    # s3.put_object(Bucket=BUCKET_NAME, Key=INPUT_FILE_NAME, Body=event[PARAM_FILE_CONTENT_KEY])
-
     # with open(INPUT_FILE_NAME, 'w') as input_file:
     #     input_file.write(event[PARAM_FILE_CONTENT_KEY])
 
-    convert_html_to_pdf()
+    result = convert_html_to_pdf()
 
     # command = f'ls -l'
     # args = shlex.split(command)
     # subprocess.run(args)
+
+    s3 = boto3.resource("s3")
+    s3.put_object(Bucket=BUCKET_NAME, Key=INPUT_FILE_NAME, Body=result)
 
     # return {
     #     "statusCode": 200,
@@ -44,18 +44,21 @@ def generate_pdf_from_html():
     #     })
     # }
 
+
+
 def convert_html_to_pdf():
     # converter.convert('https://pypi.org', 'sample_scale_0_5.pdf', print_options={"scale": 0.5})
     path = os.path.abspath('sample_website.html')
     source = f'file:///{path}'
-    target = 'sample_scale_XXXX.pdf'
+    target = 'sample_scale_test_XXXX.pdf'
     print_options = {
         "scale" : 1
     }
     # converter.convert(source=source, target='sample_scale_2.pdf', print_options={"scale": 1})
     result = get_pdf_from_html(source=source, print_options=print_options)
-    with open(target, "wb") as file:
-        file.write(result)
+    # with open(target, "wb") as file:
+    #     file.write(result)
+    return result
 
 def get_pdf_from_html(
         source: str,
